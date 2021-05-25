9d9bce914a7605c75a644cf45e8cb6cb0eb2fdee
SharatMehrotra <47215289+SharatMehrotra@users.noreply.github.com>
2021-05-25 18:00:45 +0530

    Update CodeforcesContestList.py
    Added the contest run time

--- CodeforcesContestList.py
+++ CodeforcesContestList.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+
 URL = "https://codeforces.com/contests"
 r = requests.get(URL)
 soup = BeautifulSoup(r.content, 'html5lib')
@@ -13,6 +14,7 @@ count=0
 file3=open("SCHEDULE","w")
 name=[]
 date=[]
+time=[]
 for row in table.findAll('tr'):
     if(row.has_attr('data-contestid')):
         count+=1
@@ -20,7 +22,10 @@ for row in table.findAll('tr'):
         for row1 in row.findAll('td'):
             if(row1.find('span') and row1.span.has_attr('data-locale')):
                 date.append(row1.span.text)
+            elif(not(row1.find('span')) and not(row1.has_attr('span')) and (':' in row1.text) and row1.text!=row.td.text):    
+                time.append(row1.text)
 newname=[]
+print(time)
 for i in range(len(name)):
     name[i] = name[i].split()
     x=0
@@ -31,6 +36,8 @@ for i in range(len(name)):
     newname.append(st)
 for i in range(len(name)):
     s1="Contest Name: " + newname[i]+"\n"
-    s2="Contest Date: " + date[i]+"\n\n"
+    s2="Contest Date: " + date[i]+"\n"
+    s3="Contest Run Time: "+time[i].strip()+"\n\n"
     file3.write(s1)
     file3.write(s2)
+    file3.write(s3)
